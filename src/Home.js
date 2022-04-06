2b9eec811950d15d63c78e9bbb67107940ba338e
Usman <bkayani50@gmail.com>
2022-04-06 18:05:40 +0500

    presale testing

--- src/Home.js
+++ src/Home.js
@@ -31,7 +31,6 @@ const Home = ({ connecctstatus, setConnectedstatus }) => {
 	}, []);
 
 	async function loadWeb3() {
-		connectWallet();
 		if (window.ethereum) {
 			window.web3 = new Web3(window.ethereum);
 			// await window.ethereum.enable();
