73a5c65c28d85c07b45f6d03cd72394fdadf14ad
Usman <bkayani50@gmail.com>
2022-08-29 11:07:31 +0500

    dapp ready for minting

--- src/Home.js
+++ src/Home.js
@@ -221,6 +221,24 @@ const Home = ({ connecctstatus, setConnectedstatus }) => {
 									</svg>{" "}
 									20% discount code to use on our forjaessc.com/shop
 								</div>
+								<div>
+									<svg
+										width="24px"
+										height="24px"
+										viewBox="0 0 24 24"
+										className="mb-1"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path d="M16.813 4.419A.997.997 0 0 0 16 4H3a1 1 0 0 0-.813 1.581L6.771 12l-4.585 6.419A1 1 0 0 0 3 20h13a.997.997 0 0 0 .813-.419l5-7a.997.997 0 0 0 0-1.162l-5-7z" />
+									</svg>{" "}
+									One year membership at "Socio International" level, which
+									includes ability to vote during presidential elections and
+									even run as president of Forjães SC (more info here:{" "}
+									<a href="https://forjaessc.com/memberships/">
+										https://forjaessc.com/memberships/
+									</a>{" "}
+									)
+								</div>
 							</div>
 						</div>
 						<div className="container text-center d-flex justify-content-center py-5">
